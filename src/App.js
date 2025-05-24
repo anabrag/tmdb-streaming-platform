1409f8b2cdaceac658689abf666e9e83f7bc898f
Ana Caroline <caroline.ma.braganca@gmail.com>
2025-05-24 00:20:15 -0300

    chore: setup project with RSuite and folder structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Container, Header, Content, Footer } from 'rsuite';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Header><h2 style={{ padding: '1rem' }}>Movie Trailers App</h2></Header>
+      <Content style={{ padding: '1rem' }}>
+        Conteúdo inicial
+      </Content>
+      <Footer style={{ textAlign: 'center', padding: '1rem' }}>
+        © 2025
+      </Footer>
+    </Container>
   );
 }
 
